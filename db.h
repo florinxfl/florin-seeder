da99a56b75ccb34d890c1b3bc3564325a72155c5
Malcolm MacLeod <malcolm@gulden.com>
2020-09-08 08:24:17 +0200

    Bump seeder block height, for post delta v3 activation

--- db.h
+++ db.h
@@ -17,7 +17,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 500000 : 1132150;
+    return testnet ? 500000 : 1227800;
 }
 
 std::string static inline ToString(const CService &ip) {
